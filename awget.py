e011a3cfb143d7fbc92729c6ec74afe5176f6daf
jzfraser <49706249+jzfraser@users.noreply.github.com>
2020-11-11 11:03:37 -0700

    Upload files with progress made
    awget.py and ss.py are unfinished, but partially done. txt files have placeholder information to check functionality of reading files.

---
+++ awget.py
@@ -0,0 +1,58 @@
+import socket, sys, random
+
+fileName = "chaingang.txt"
+url = sys.argv[1]
+ssChain = []
+numSS = 0
+
+if (len(sys.argv) == 4):
+    option = sys.argv[2]
+    value = sys.argv[3]
+    if (option == "-c"):
+        fileName = value
+    else:
+        print("Invalid chainfile name, exiting")
+        sys.exit(1)
+elif (len(sys.argv) != 2):
+    print("Invalid arguments, exiting")
+    sys.exit(1)
+
+try:
+    with open(fileName, "r") as chainfile:
+        numSS = int(chainfile.readline())
+        for line in chainfile:
+            if (line == "" or line == "\n"):
+                break
+            info = line.split()
+            if (len(info) != 2):
+                raise Exception("")
+            ssAddr = info[0]
+            ssPort = info[1]
+            ssChain.append((ssAddr, ssPort))
+except:
+    print("Unable to read in chainfile, exiting")
+    sys.exit(1)
+
+print("Request: %s" % url)
+print("Chainlist is")
+for ss in ssChain:
+    print("<%s, %s>" % (ss[0], ss[1]))
+
+choice = randrange(numSS - 1)
+ssAddr = ssChain[choice]
+numSS -= 1
+ssChain.pop(choice)
+ssSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+try:
+    ssSock.connect(ssAddr)
+    numSS -= 1
+    ssChain.pop(choice)
+except:
+    print("Connection to stepping stone failed, exiting")
+    sys.exit(1)
+
+print("Waiting for file...")
+
+
+# print("Received file %s" % outFileName)
+
